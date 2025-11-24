a65096f8edea9e9d1a4991984db614fdc2b46480
Denis <alexdefult@gmail.com>
2025-11-24 17:16:42 +0700

    feat(articles): add article status, add hook useState for searchQuery

--- src/features/articles/ArticlesPage.tsx
+++ src/features/articles/ArticlesPage.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 import type { Article } from "../../domain/models";
 
@@ -6,15 +7,35 @@ type ArticlesPageProps = {
   articles: Article[];
 };
 
+type ArticleStatusRussian = "актуальна" | "частично устарела" | "требует обновления";
+
+const ArticleStatusMap: Record<Article["status"], ArticleStatusRussian> = {
+  actual: "актуальна",
+  partially_outdated: "частично устарела",
+  needs_update: "требует обновления",
+};
+//TODO: Попрактиковать артиклстатусмап и отобразить статус статьи на русском языке
 export function ArticlesPage({ articles }: ArticlesPageProps) {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+
+
   return (
+    
     <div>
+      <input 
+      value={searchQuery}
+      type="text"
+      placeholder="Поиск по заголовку..."
+      onChange={(event) => {setSearchQuery(event.target.value)}}/>
       <h1>Список статей</h1>
 
       {articles.map((article) => (
         <div key={article.id}>
           <h2>{article.title}</h2>
-          <p>status: {article.status}</p>
+          <p>статус: {ArticleStatusMap[article.status]}</p>
         </div>
       ))}
     </div>
--- src/mocks/articles.ts
+++ src/mocks/articles.ts
@@ -12,6 +12,16 @@ export const articlesMock: Article[] = [
     title: "Страховой Агент",
     status: "actual",
     currentVersionId: "2",
+},
+{    id: "3",
+    title: "Фискальный регистратор",
+    status: "partially_outdated",
+    currentVersionId: "3",
+},
+{    id: "4",
+    title: "Онлайн касса",
+    status: "needs_update",
+    currentVersionId: "4",
 }
 ]
 
@@ -31,5 +41,19 @@ export const articleVersionMocks: ArticleVersion[] = [
     createdAt: "14.06.2025",
     createdByUserId: "318",
     appVersion: "1.6.1"
+},
+{    id: "3",
+    articleId: "3",
+    body: "Фискальный регистратор - устройство для печати чеков",
+    createdAt: "20.06.2025",
+    createdByUserId: "318",
+    appVersion: "2.0.0"
+},
+{    id: "4",
+    articleId: "4",
+    body: "Онлайн касса - это современное решение для вашего бизнеса",
+    createdAt: "25.06.2025",
+    createdByUserId: "318",
+    appVersion: "3.1.4"
 }
 ]
