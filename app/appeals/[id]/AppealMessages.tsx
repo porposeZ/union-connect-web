fd045fe3339626dbbcca947e39302e2bd9ad804f
porposeZ <klimovskiy-1997@mail.ru>
2026-01-22 17:56:45 +0700

    main: add AppealMessagesServerComponent

---
+++ app/appeals/[id]/AppealMessages.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useState } from "react";
+import { Appeal, AppealMessage } from "@/src/domain/models";
+
+type AppealMessagesProps = {
+    appeal: Appeal;
+    messages: AppealMessage[];
+}
+
+export default function AppealMessages({appeal, messages}: AppealMessagesProps) {
+    const [showInternal, setShowInternal] = useState(false)
+    const visibleMessages = showInternal
+        ? messages
+        : messages.filter((m) => !m.isInternal)
+
+
+
+    return (
+        <div className="min-h-screen bg-zinc-50 p-6 dark:bg-black">
+            <h1 className="mb-4 text-xl font-semibold">Обращение #{appeal.number}</h1>
+            <h1 className="">Тема: {appeal.theme} </h1>
+            <button
+                className="rounded-lg border bg-white p-4"
+                onClick={() => setShowInternal((prev) => !prev)}
+            >
+                {showInternal ? "Скрыть внутренние" : "Показать внутренние"}
+            </button>
+            <h2 className="mb-4 text-lg font-semibold">Сообщения:</h2>
+            {visibleMessages.map((message) => (
+                <div key={message.id} className="mb-4 rounded-lg border bg-white p-4">
+                    <div className="mb-2 text-sm text-gray-500">
+                        От: {appeal.clientName} · {message.createdAt} · {message.authorType}
+                        <div className="text-pink-800 mt-2">
+                            <p>"{message.message}"</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
--- app/appeals/[id]/page.tsx
+++ app/appeals/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { appealsMocks, appealMessageMocks } from "@/src/mocks/articles"
 import { notFound } from "next/navigation";
+import AppealMessages from "./AppealMessages";
 
 function parseRuDateTime(value: string): number {
-  const [datePart, timePart] = value.trim().split(" ");
-  const [day, month, year] = datePart.split(".").map(Number);
-  const [hour = 0, minute = 0] = timePart ? timePart.split(":").map(Number) : [];
-  return new Date(year, month - 1, day, hour, minute).getTime();
+    const [datePart, timePart] = value.trim().split(" ");
+    const [day, month, year] = datePart.split(".").map(Number);
+    const [hour = 0, minute = 0] = timePart ? timePart.split(":").map(Number) : [];
+    return new Date(year, month - 1, day, hour, minute).getTime();
 }
 
 export default async function AppealPage({
@@ -13,34 +14,17 @@ export default async function AppealPage({
 }: {
     params: Promise<{ id: string }>;
 }) {
-    const {id} = await params;
+    const { id } = await params;
     const appeal = appealsMocks.find((appeal) => appeal.id === id)
     if (!appeal) {
         return notFound();
     }
-    const messages = appealMessageMocks.filter((m) => m.appealId === appeal.id);
-
-
-    const sortedMessages = [...messages].sort(
-        (a, b) => parseRuDateTime(a.createdAt) - parseRuDateTime(b.createdAt)    
+    const messages = appealMessageMocks.filter((m) => m.appealId === appeal.id)
+    .sort(
+    (a, b) => parseRuDateTime(a.createdAt) - parseRuDateTime(b.createdAt)
     )
-
+    
     return (
-    <div className="min-h-screen bg-zinc-50 p-6 dark:bg-black">
-        <h1 className="mb-4 text-xl font-semibold">Обращение #{appeal.number}</h1>
-        <h1 className="">Тема: {appeal.theme} </h1>
-        <button className="rounded-lg border bg-white p-4">Кнопка</button>
-        <h2 className="mb-4 text-lg font-semibold">Сообщения:</h2>
-        {sortedMessages.map((message) => (
-            <div key={message.id} className="mb-4 rounded-lg border bg-white p-4">
-                <div className="mb-2 text-sm text-gray-500">
-                    От: {appeal.clientName} · {message.createdAt} · {message.authorType}
-                    <div className="text-pink-800 mt-2">
-                    <p>"{message.message}"</p>
-                    </div>
-                </div>
-            </div>
-        ))}
-        </div>
-)
+        <AppealMessages appeal={appeal} messages={messages}/>
+    )
 }
